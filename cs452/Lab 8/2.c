1858dda53ee82d7c076b8bac8164a90880ae71ab
iJEEBUS <ronrounsifer@me.com>
2019-03-28 13:37:00 -0400

    Began working on lab 9

--- cs452/Lab 8/2.c
+++ cs452/Lab 8/2.c
@@ -1,11 +1,25 @@
+/**
+* Lab 8 - Q. 2 - GVSU
+* 
+* Experimenting with memory management.
+*
+* @author Ron Rounsifer
+**/
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 int init_global = 5235;
 int uninit_global;
-int g_array[50];
-char *g_ptr;
+int g_array_uninit[50];
+int g_array_init[3] = {1,2,3};
+
+
+// add a struct
+struct node {
+    int value;
+};
+
 
 int main() {
 
@@ -15,21 +29,41 @@ int main() {
     char *l_ptr = malloc(sizeof(char));
     int local = 555;
     int uninit_local;
-    
-    g_ptr = malloc(sizeof(char));
-    printf("Dynamic\n");
-    printf("global array:     %x     %X    %p\n", g_array, g_array, &g_array);
-    printf("init global:      %x     %X    %p\n", init_global, init_global, &init_global);
-    printf("uninit global:      %x     %X    %p\n", uninit_global, uninit_global, &uninit_global);
-    printf("global pointer:    %x     %X     %p\n", g_ptr, g_ptr, g_ptr);
-    printf("local pointer:     %x     %X     %p\n", l_ptr, l_ptr, l_ptr);
-    printf("\n");
-    printf("Stack\n");
-    printf("init local:       %x     %X     %p\n", local, local, &local);
-    printf("uninit local:       %x     %X     %p\n", uninit_local, uninit_local, &uninit_local);
-    function_variable();
-    
+    int fact = factorial(5);
+    struct node test;
 
+    printf("Code\n");
+    printf("factorial is stored at: %p\n\n", factorial);
+
+    
+    printf("Stack\n");
+    printf("local init stored at:    %p\n", &local);
+    printf("local uninit stored at:    %p\n", &uninit_local);
+    printf("local pointer stored at:   %px\n", &l_ptr);
+    printf("fact stored at: %p\n", &fact);
+    function_variable();
+    printf("node structure located at: %p\n\n", &test);
+    
+    printf("\n\n");
+    printf("Heap\n");
+    printf("Run time data:\n");
+    printf("local pointers data stored at:   %p\n\n", l_ptr);
+    
+    printf("Run time libraries:\n");
+    printf("printf() function located at: %p", printf);
+
+    printf("\n\n");
+    printf("Data\n");
+    printf("global array uninit:   %p\n",  &g_array_uninit);
+    printf("uninit global:        %p\n",  &uninit_global);
+   
+    printf("\n\n");
+    printf("Init Data\n") ;
+    printf("init global:    %p\n",  &init_global); 
+    printf("global array init:   %p\n", &g_array_init);
+    
+    printf("\n");
+    
     
 
 return 0;
@@ -39,9 +73,13 @@ return 0;
 void function_variable()
 {
     int function = 2483;
-    g_ptr = malloc(sizeof(char));
-    printf("function stack:     %x    %X     %p\n", function, function, &function);
-    printf("function heap:      %x    %X     %p\n", g_ptr, g_ptr, &g_ptr);
+    printf("function stack:     %p\n",  &function);
 };
 
-
+int factorial(int n) {
+    if (n == 0)
+    {
+        return n;
+    }
+    return n * factorial(n-1);
+}
