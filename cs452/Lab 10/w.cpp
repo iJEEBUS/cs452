c973d0a43412bdf912b4c9a80cd1d04e9f37501c
Ronald Rounsifer <ronrounsifer@me.com>
2019-04-12 10:29:33 -0400

    Tidying up code / finalizing last few errors.

--- cs452/Lab 10/w.cpp
+++ cs452/Lab 10/w.cpp
@@ -1,3 +1,6 @@
+// lab10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 /************************************************************************************************
 * Lab 10 - CIS 452
 *
@@ -10,7 +13,7 @@
 #include <iostream>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
 	// Page size
 	SYSTEM_INFO sys_info;
@@ -18,48 +21,48 @@ int main()
 	std::cout << "Page size: " << sys_info.dwPageSize << " Bytes" << std::endl;
 
 	// Allocate memory
-	char *allocated_mem;
-	allocated_mem = malloc(1024 * 1024);
-	std::cout << "1M bytes of memory allocated.\n\n" << std::endl;
-	
+	char* allocated_mem;
+	allocated_mem = (char*) malloc(1024 * 1024);
+	std::cout << "1M bytes of memory allocated.\n" << std::endl;
+
 	// Query system
 	// Determine state of allocated memory
 	MEMORY_BASIC_INFORMATION mem_info;
-	VirtualQuery(allocated_mem, &mem_info, sizeof(mem_info));	
+	VirtualQuery(allocated_mem, &mem_info, sizeof(mem_info));
 	switch (mem_info.State)
 	{
-		case MEM_COMMIT:
-			std::cout << "Memory state: Committed. " << std::endl;
-			break;
-		case MEM_RESERVE:
-			std::cout << "Memory state: Reserved. " << std::endl;
-			break;
-		case MEM_FREE:
-			std::cout << "Memory state: Free. " << std::endl;			
-			break;
+	case MEM_COMMIT:
+		std::cout << "Memory state: Committed. " << std::endl;
+		break;
+	case MEM_RESERVE:
+		std::cout << "Memory state: Reserved. " << std::endl;
+		break;
+	case MEM_FREE:
+		std::cout << "Memory state: Free. " << std::endl;
+		break;
 	}
 
 
 	// Free memory
-	free(str);
-	std::cout << "\n\nMemory freed.\n\n << std::endl;
+	free(allocated_mem);
+	std::cout << "\nFreeing memory...\n" << std::endl;
 
 
-	// Query system after freeing memory
-	// Determine state of allocated memory
-	VirtualQuery(allocated_mem, &mem_info, sizeof(mem_info));	
+		// Query system after freeing memory
+		// Determine state of allocated memory
+		VirtualQuery(allocated_mem, &mem_info, sizeof(mem_info));
 	switch (mem_info.State)
 	{
-		case MEM_COMMIT:
-			std::cout << "Memory state: Committed. " << std::endl;
-			break;
-		case MEM_RESERVE:
-			std::cout << "Memory state: Reserved. " << std::endl;
-			break;
-		case MEM_FREE:
-			std::cout << "Memory state: Free. " << std::endl;			
-			break;
+	case MEM_COMMIT:
+		std::cout << "Memory state: Committed. " << std::endl;
+		break;
+	case MEM_RESERVE:
+		std::cout << "Memory state: Reserved. " << std::endl;
+		break;
+	case MEM_FREE:
+		std::cout << "Memory state: Free. " << std::endl;
+		break;
 	}
-	
+
 	return 0;
 }
